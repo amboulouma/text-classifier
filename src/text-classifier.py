94be3da57f1cc55972f4a3efbe20a1a92fdfd962
Amine M. Boulouma <amine@boulouma.com>
2018-11-24 22:00:50 +0100

    Corrected tokenizing to avoid token 0

--- src/text-classifier.py
+++ src/text-classifier.py
@@ -28,6 +28,8 @@ tokenized_features = {}
 for i in range(len(tokenized_features_list)):
     tokenized_features[tokenized_features_list[i]] = i + 1
 
+pprint(tokenized_features)
+
 ### Replacing worlds with tokens
 
 tokenized_features_list = []
@@ -36,9 +38,38 @@ for feature in feature_list:
     tokenized_feature = []
     for world in feature.split():
         tokenized_feature.append(tokenized_features[world])
-    tokenized_features_list.append(tuple(tokenized_feature))
+    tokenized_features_list.append(tokenized_feature)
 
 ## Padding the vectors so that they will have the same length 
 
-pprint(tokenized_features_list)
+### Having the max length
+
+max_length = len(max(tokenized_features_list, key=len))
+
+for vector in tokenized_features_list:
+    while len(vector) < 6:
+        vector.append(0)
+
+# Tokenizing target vectors and link them the tokenized features
+
+labels_list = list(data["labels"])
+
+tokenized_labels = {
+    "yes": 1,
+    "no": 0,
+    "maybe": 0.5
+}
+
+# pprint(labels_list)
+# pprint(tokenized_features_list)
+
+
+
+# tokenized_data_set = {}
+
+# for tokenized_feature, label in tokenized_features_list, labels_list:
+#     tokenized_data_set[tokenized_feature] = tokenized_labels[label]
+
+
+# pprint(tokenized_data_set)
 
