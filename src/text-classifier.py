d1237234014bdddb2cb64bb2569cbf18ae9a85e1
Amine M. Boulouma <amine@boulouma.com>
2018-11-24 21:34:21 +0100

    Tokenized features

--- src/text-classifier.py
+++ src/text-classifier.py
@@ -6,7 +6,11 @@ from pprint import pprint
 
 data = pd.read_csv("data.csv")
 
-# Tokenize the features 
+# Parsing the features
+
+## Tokenize the features 
+
+## Generate tokens for worlds
 
 feature_list = list(data["features"])
 tokenized_features_list = []
@@ -16,12 +20,23 @@ for feature in feature_list:
 
 tokenized_features_list = list(chain.from_iterable(tokenized_features_list))
 
-## Tokenized items have to be unique
+### Tokenized items have to be unique
 
 tokenized_features_list = list(set(tokenized_features_list))
 
 tokenized_features = {}
 for i in range(len(tokenized_features_list)):
-    tokenized_features[i] = tokenized_features_list[i]
+    tokenized_features[tokenized_features_list[i]] = i
 
-pprint(tokenized_features)
+### Replacing worlds with tokens
+
+tokenized_features_list = []
+
+for feature in feature_list:
+    tokenized_feature = []
+    for world in feature.split():
+        tokenized_feature.append(tokenized_features[world])
+    tokenized_features_list.append(tuple(tokenized_feature))
+
+pprint(tokenized_features_list)
+
