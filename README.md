d3ba8d1452da9a2e09fff97b866f270b2224f55f
Amine M. Boulouma <amine@boulouma.com>
2018-11-25 01:08:34 +0100

    Added ressources

---
+++ README.md
@@ -0,0 +1,5 @@
+# Text Classifier
+
+## Ressources
+
+[How to build a neural network with Keras](https://towardsdatascience.com/how-to-build-a-neural-network-with-keras-e8faa33d0ae4)
--- src/text-classifier.py
+++ src/text-classifier.py
@@ -1,10 +1,14 @@
 from itertools import chain
-import pandas as pd
 from pprint import pprint
+
+import pandas as pd
+import numpy as np
 
 from keras.utils import to_categorical
 from keras import models
 from keras import layers
+from keras.datasets import imdb
+
 
 # Import the data
 
@@ -69,12 +73,18 @@ tokenized_labels_list = []
 for label in labels_list:
     tokenized_labels_list.append(tokenized_labels[label])
 
-# pprint(tokenized_features_list)
-# pprint(tokenized_labels_list)
+training_data = np.array(tokenized_features_list)
+training_targets = np.array(tokenized_labels_list)
 
 testing_data = [[6, 30, 16, 10, 21, 0],
                 [6, 30, 16, 10, 21, 0],
                 [6, 30, 74, 0, 0, 0],
                 [6, 30, 74, 0, 0, 0]]
 
-testing_output = [0.5, 0.5, 0.5, 0.5]
+testing_targets = [0.5, 0.5, 0.5, 0.5]
+
+data = np.concatenate((training_data, testing_data), axis=0)
+targets = np.concatenate((training_targets, testing_targets), axis=0)
+
+print("Categories:", np.unique(targets))
+print("Number of unique words:", len(np.unique(np.hstack(data))))
